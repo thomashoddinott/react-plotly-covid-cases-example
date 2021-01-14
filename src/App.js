7d0924c1ebf231d2d451af7a14a41eb1b3128021
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-01-14 20:46:33 +0000

    fin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import Plotly from "plotly.js"
+import createPlotlyComponent from 'react-plotly.js/factory';
+const Plot = createPlotlyComponent(Plotly);
 
 function App() {
+
+  const [coronaData, setCoronaData] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://api.covid19api.com/dayone/country/italy/status/confirmed`)
+      .then(res => {
+        setCoronaData(res.data)
+      }).catch((error) => console.log(`API get failed — ${error}`))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Plot
+        data={[
+          { type: 'line', x: coronaData.map(m => m.Date), y: coronaData.map(m => m.Cases) },
+        ]}
+        layout={{ width: 1200, height: 600, title: 'Italy - Covid-19 Total Cases' }}
+      />
     </div>
   );
 }
